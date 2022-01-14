e40edf4152c21d9773c3209f889225e05df31282
DevTec11 <89366079+DevTec11@users.noreply.github.com>
2022-01-13 17:54:05 -0700

    not sure what this is

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Is JS working?");
+document.getElementById('nickname').textContent = "Doc";
+document.getElementById('favorites').textContent = "Streetwear";
+document.getElementById('hometown').textContent = "Oakland";
